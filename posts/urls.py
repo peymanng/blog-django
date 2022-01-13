9bb8207008408125fac7b5d77d3c2f89d302fab7
Peyman Golkar <peymanpic00@gmail.com>
2022-01-13 09:19:04 +0330

    fix like url

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path ,re_path
 from . import views
 urlpatterns = [
     path("", views.posts, name="posts"),
+    re_path(r"like/(?P<post_slug>[-\w]+)",views.like, name="like"),
     re_path(r"post/(?P<post>[-\w]+)", views.post, name="post"),
     re_path(r"(?P<tag_slug>[-\w]+)", views.posts, name="posts_tag"),
-    re_path(r"like/(?P<post_slug>[-\w]+)",views.like, name="like")
 ]
