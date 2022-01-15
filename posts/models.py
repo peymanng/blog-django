d0c0a9f7420700a9dae56ed3570b7552210bf651
Peyman Golkar <peymanpic00@gmail.com>
2022-01-15 20:55:41 +0330

    add __str__ to category model

--- posts/models.py
+++ posts/models.py
@@ -21,6 +21,9 @@ class Category(models.Model):
     
     class Meta:
         verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
